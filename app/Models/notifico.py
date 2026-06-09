db6b7b07a87041b241f9d309c3a97b01a2865a38
federicam-05 <fedemozzo2005@gmail.com>
2026-06-09 17:00:55 +0200

    notifica modifica

---
+++ app/Models/notifico.py
@@ -0,0 +1,9 @@
+
+    class Notifica:
+        def __init__(self, destinatario, messaggio, letta, tipo, richiestaId = None, id = None):
+        self.id = id
+        self.destinatario = destinatario
+        self.messaggio = messaggio
+        self.letta = letta
+        self.tipo = tipo
+        self.richiestaId = richiestaId

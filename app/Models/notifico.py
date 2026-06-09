c0559b183eed79b049b1c7a70e921229d10e69b5
federicam-05 <fedemozzo2005@gmail.com>
2026-06-09 17:14:02 +0200

    modifica

--- app/Models/notifico.py
+++ app/Models/notifico.py
@@ -1,6 +1,6 @@
 
     class Notifica:
-        def __init__(self, destinatario, messaggio, letta, tipo, richiestaId = None, id = None):
+    def __init__(self, destinatario, messaggio, letta, tipo, richiestaId = None, id = None):
         self.id = id
         self.destinatario = destinatario
         self.messaggio = messaggio

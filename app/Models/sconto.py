a6dd51d45c022bfe3337064aa2372d6aeb6ee20a
federicam-05 <fedemozzo2005@gmail.com>
2026-06-09 17:21:53 +0200

    modifica z

--- app/Models/sconto.py
+++ app/Models/sconto.py
@@ -1,10 +1,10 @@
 class sconto:
 
-    def __init__(self,id,evento,percentuale,dataInicio,dataFine):
+    def __init__(self,id,evento,percentuale,dataInizio,dataFine):
         self.id = id
         self.evento = evento
         self.percentuale = percentuale
-        self.dataInicio = dataInicio
+        self.dataInizio = dataInizio
         self.dataFine = dataFine
 
 

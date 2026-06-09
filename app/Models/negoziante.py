6b256546c6869652689e21bd33a14b20e68ca1fb
Samepsilon <164898991+Samepsilon@users.noreply.github.com>
2026-06-09 16:57:27 +0200

    Utente cliente negoziante implementation

---
+++ app/Models/negoziante.py
@@ -0,0 +1,6 @@
+from utente import Utente
+
+class Negoziante(Utente):
+
+    def __init__(self, username, email, password, id=None):
+        super().__init__(username, email, password, ruolo="negoziante", id=id)
